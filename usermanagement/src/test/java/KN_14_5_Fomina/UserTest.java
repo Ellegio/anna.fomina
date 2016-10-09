fe74c0134e1cb60ecfbff401247f604141e86430
AnnaFomina <anna.fomina@nure.ua>
2016-10-09 20:28:16 +0300

    Added project

---
+++ usermanagement/src/test/java/KN_14_5_Fomina/UserTest.java
@@ -0,0 +1,33 @@
+package KN_14_5_Fomina;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+public class UserTest extends TestCase {
+
+	private User user;
+	private Date dateOfBirth; 
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		user = new User();
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1982, Calendar.AUGUST, 21);
+		dateOfBirth = calendar.getTime();
+	}
+	
+	public void testGetFullName() {
+		user.setFirstName("Tyrell");
+		user.setLastName("Wellik");
+		assertEquals("Tyrell, Wellik", user.getFullName());
+	}
+	
+	public void testGetAge() {
+		user.setDateOfBirth(dateOfBirth);
+		assertEquals(2016 - 1982, user.getAge());
+	}
+
+}
